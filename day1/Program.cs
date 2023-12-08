b57bce42c992ad72f6b2063331b205b240be5c66
Martin Klinke <info@martinklinke.com>
2023-12-08 20:52:31 +0100

    Day 1 - Part 2

---
+++ day1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Day 1");
+
+var numbers = new[] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+var allLines = File.ReadAllLines("input.txt");
+
+var sum = 0;
+
+foreach (var line in allLines)
+{
+    var characterIndex = 0;
+    var digits = new List<int>();
+    foreach (var character in line)
+    {
+        if (character >= 48 && character <= 57) {
+            digits.Add(((int)character)-48);
+        }
+        var numberIndex = 0;
+        foreach(var number in numbers)
+        {
+            numberIndex++;
+            if (line[characterIndex..].StartsWith(number)) 
+            {
+                digits.Add(numberIndex);
+            }
+        }
+        characterIndex++;
+    }
+
+    var calibrationValue = digits.First() + "" + digits.Last();
+    sum += int.Parse(calibrationValue);
+}
+
+Console.WriteLine("Sum: "+sum);
