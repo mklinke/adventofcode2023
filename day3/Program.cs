2fa104de4c8f716ecb3c22432b698cd4f956f97e
Martin Klinke <info@martinklinke.com>
2023-12-09 21:33:18 +0100

    Day 3 - Part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -2,67 +2,79 @@
 
 var allLines = new LinkedList<string>(File.ReadAllLines("input.txt"));
 
-var sum = 0;
-for (var node = allLines.First; node != null; node = node.Next)
+var stars = new Dictionary<Tuple<int, int>, List<int>>();
+var lineIndex = 0;
+for (var node = allLines.First; node != null; node = node.Next, lineIndex++)
 {
     var previousNode = node.Previous;
     var nextNode = node.Next;
-    var numbers = new List<int>();
     for (var i = 0; i < node.Value.Length; i++)
     {
         var character = node.Value[i];
-        var currentNumber = "";
+        var currentNumberString = "";
         var startIndex = i;
         while (character >= 48 && character <= 57)
         {
-            currentNumber += character;
+            currentNumberString += character;
             if (i == node.Value.Length - 1) { break; }
             character = node.Value[++i];
         }
-
-        if (currentNumber.Length > 0)
+        if (currentNumberString.Length > 0)
         {
-            var endIndex = startIndex + currentNumber.Length - 1;
-            var adjacentToSpecialChar = false;
+            var currentNumber = int.Parse(currentNumberString);
+            var endIndex = startIndex + currentNumberString.Length - 1;
             for (var checkIndex = Math.Max(0, startIndex - 1); checkIndex < Math.Min(endIndex + 2, node.Value.Length); checkIndex++)
             {
-                if (previousNode != null && previousNode.Value[checkIndex] != '.' && (previousNode.Value[checkIndex] < 48 || previousNode.Value[checkIndex] > 57))
+                if (previousNode != null && previousNode.Value[checkIndex] == '*')
                 {
-                    adjacentToSpecialChar = true;
+                    var coordinates = new Tuple<int, int>(lineIndex - 1, checkIndex);
+                    AddAdjacentNumber(stars, currentNumber, coordinates);
                 }
-                if (nextNode != null && nextNode.Value[checkIndex] != '.' && (nextNode.Value[checkIndex] < 48 || nextNode.Value[checkIndex] > 57))
+                if (nextNode != null && nextNode.Value[checkIndex] == '*')
                 {
-                    adjacentToSpecialChar = true;
-                }
-                if (adjacentToSpecialChar)
-                {
-                    break;
+                    var coordinates = new Tuple<int, int>(lineIndex + 1, checkIndex);
+                    AddAdjacentNumber(stars, currentNumber, coordinates);
                 }
             }
 
             if (startIndex > 0)
             {
-                if (node.Value[startIndex - 1] != '.' && (node.Value[startIndex - 1] < 48 || node.Value[startIndex - 1] > 57))
+                if (node.Value[startIndex - 1] == '*')
                 {
-                    adjacentToSpecialChar = true;
+                    var coordinates = new Tuple<int, int>(lineIndex, startIndex - 1);
+                    AddAdjacentNumber(stars, currentNumber, coordinates);
                 }
             }
 
             if (endIndex < node.Value.Length - 1)
             {
-                
-                if (node.Value[endIndex + 1] != '.' && (node.Value[endIndex + 1] < 48 || node.Value[endIndex + 1] > 57))
+                if (node.Value[endIndex + 1] == '*')
                 {
-                    adjacentToSpecialChar = true;
+                    var coordinates = new Tuple<int, int>(lineIndex, endIndex + 1);
+                    AddAdjacentNumber(stars, currentNumber, coordinates);
                 }
             }
-            if (adjacentToSpecialChar)
-            {
-                sum += int.Parse(currentNumber);
-            }
-            currentNumber = "";
         }
     }
 }
 
+var sum = 0;
+
+foreach (var star in stars.Where((entry) => entry.Value.Count == 2))
+{
+    sum += star.Value.Aggregate((a, b) => a * b);
+}
+
 Console.WriteLine("Sum: " + sum);
+
+static void AddAdjacentNumber(Dictionary<Tuple<int, int>, List<int>> stars, int currentNumber, Tuple<int, int> coordinates)
+{
+    if (stars.TryGetValue(coordinates, out var adjacentNumbers))
+    {
+        adjacentNumbers.Add(currentNumber);
+    }
+    else
+    {
+        stars.Add(coordinates, new List<int> { currentNumber });
+    }
+}
