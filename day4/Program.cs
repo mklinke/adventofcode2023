e424565b72a4d74f394e2b3df4cbe400a6e96b53
Martin Klinke <info@martinklinke.com>
2023-12-11 22:31:22 +0100

    Day 4 - Part 2

--- day4/Program.cs
+++ day4/Program.cs
@@ -2,28 +2,35 @@
 
 var allLines = File.ReadAllLines("input.txt");
 
+var cardCopies = new Dictionary<int, int>();
 var sum = 0;
 foreach (var line in allLines)
 {
+    var currentCard = int.Parse(line.Split(":")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
     var numberGroups = line.Split(":")[1].Trim().Split("|");
     var winningNumbers = numberGroups[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(text => int.Parse(text)).ToList();
     var ownNumbers = numberGroups[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(text => int.Parse(text)).ToList();
+    var cardInstances = 1;
+    if (cardCopies.TryGetValue(currentCard, out var copies)){
+        cardInstances += copies;
+    }     
     var hits = 0;
     foreach (var winningNumber in winningNumbers)
     {
         if (ownNumbers.Contains(winningNumber))
         {
-            hits += 1;
+            hits++;
+            if (!cardCopies.ContainsKey(currentCard + hits))
+            {
+                cardCopies[currentCard + hits] = cardInstances;
+            }
+            else
+            {
+                cardCopies[currentCard + hits]+=cardInstances;
+            }
         }
     }
-    if (hits > 0)
-    {
-        var value = 1;
-        for (var i = 0; i < hits - 1; i++) {
-            value *= 2;
-        }
-        sum += value;
-    }
+    sum += cardInstances;
 }
 
 Console.WriteLine("Sum: " + sum);
